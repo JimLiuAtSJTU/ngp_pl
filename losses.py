use amsgrad to fix the numerical issue(probably and hopefully? ) but with subtle performance and quality downgrade. nevertheless, suds-like background field may be more important to implement.

losses.py
@@ -74,7 +74,7 @@ class NeRFLoss(nn.Module):
 
         o = results['opacity']+1e-10
 
-        opacity_dynamic=results['opacity_dynamic']
+        opacity_dynamic=results['opacity_dynamic'] + 1e-10
 
         sigma_entropy = results['sigma_entropy']
         # encourage opacity to be either 0 or 1 to avoid floater
@@ -89,6 +89,8 @@ class NeRFLoss(nn.Module):
                                      results['ts'], results['rays_a'])
         d['entropy']=entropyloss*self.lambda_entropy
         #d['dynamic']=  torch.sum(torch.abs(1-static_weight))*self.lambda_entropy
+
+
         return d
 
 '''

models/networks_dynamic_plus.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 from .rendering import NEAR_DISTANCE
 from kornia.utils.grid import create_meshgrid3d
 
+from .debug_utils import nan_check
 
 class NGP_time_code(nn.Module):
     def __init__(self, scale, rgb_act='Sigmoid'):
@@ -58,8 +59,8 @@ class NGP_time_code(nn.Module):
             n_input_dims=64, n_output_dims=48,
             network_config={
                 "otype": "FullyFusedMLP",
-                "activation": "ReLU",
-                "output_activation": "ReLU",
+                "activation": "LeakyReLU",
+                "output_activation": "Squareplus",
                 "n_neurons": 64,
                 "n_hidden_layers": 2,
             }
@@ -106,8 +107,8 @@ class NGP_time_code(nn.Module):
                 },
                 network_config={
                     "otype": "FullyFusedMLP",
-                    "activation": "ReLU",
-                    "output_activation": "None",
+                    "activation": "LeakyReLU",
+                    "output_activation": "Squareplus",
                     "n_neurons": 64,
                     "n_hidden_layers": 1,
                 }
@@ -163,7 +164,7 @@ class NGP_time_code(nn.Module):
             n_input_dims=input_dims, n_output_dims=output_dims,
             network_config={
                 "otype": "FullyFusedMLP",
-                "activation": "ReLU",
+                "activation": "LeakyReLU",
                 "output_activation": self.rgb_act,
                 "n_neurons": 64,
                 "n_hidden_layers": 2,
@@ -213,8 +214,10 @@ class NGP_time_code(nn.Module):
             sigmas: (N)
         """
         x = (x - self.xyz_min) / (self.xyz_max - self.xyz_min)
-        h = self.encoder_static(x)
 
+        nan_check(x)
+        h = self.encoder_static(x)
+        nan_check(h)
         sigmas = TruncExp.apply(h[:, 0])
         if return_feat: return sigmas, h
         return sigmas
@@ -234,8 +237,12 @@ class NGP_time_code(nn.Module):
         # tcnn supports inputs within [0,1]
 
         t = (t-self.t_min)/(self.t_max-self.t_min)
+        nan_check(x)
+        nan_check(t)
         time_code=self.time_latent_code(t)
+        nan_check(time_code)
         h =self.xyz_t_fusion_mlp(torch.cat([static_code, time_code], 1))
+        nan_check(h)
         sigmas = TruncExp.apply(h[:, 0])
         if return_feat: return sigmas, h
         return sigmas
@@ -250,6 +257,9 @@ class NGP_time_code(nn.Module):
             sigmas: (N)
             rgbs: (N, 3)
         """
+        assert x.shape[0]>0
+        nan_check(x)
+        nan_check(d)
 
         t = kwargs.get('times')
         try:
@@ -261,6 +271,7 @@ class NGP_time_code(nn.Module):
             assert t.shape[0] == 1
 
             t = t.expand(x.shape[0], 1)
+        nan_check(t)
         # except IndexError:
         #    assert isinstance(t,torch.Tensor)
         #    print(f't,{t},{type(t)}')
@@ -270,11 +281,14 @@ class NGP_time_code(nn.Module):
 
         # use exp as activation, initialized to be 1
         sigma_static, h_static = self.static_density(x, return_feat=True)
+        nan_check(sigma_static)
 
         rgb_static = self.rgb_net_static(torch.cat([d, h_static], 1))
+        nan_check(rgb_static)
 
         # use relu as activation, initialized to be 0
         sigma_dynamic, h_dyna = self.dynamic_density(x,t,  return_feat=True)
+        nan_check(sigma_dynamic)
 
 
         #print(time_code.shape)
@@ -282,6 +296,7 @@ class NGP_time_code(nn.Module):
         #print(h_dyna.shape)
         #exit(9)
         rgb_dynamic = self.rgb_net_dynamic(torch.cat([d, h_dyna], 1))
+        nan_check(rgb_dynamic)
         extra = {
             'rgb_dynamic':rgb_dynamic[:,:3],
             'sigma_dynamic':sigma_dynamic,

models/rendering_time.py
@@ -8,7 +8,7 @@ MAX_SAMPLES = 1024
 NEAR_DISTANCE = 0.01
 
 
-
+from .debug_utils import nan_check,nan_dict_check
 
 
 
@@ -215,6 +215,14 @@ def __render_rays_train(model, rays_o, rays_d, hits_t, **kwargs):
     """
     time_grid_indx=kwargs['t_grid_indx']
     exp_step_factor = kwargs.get('exp_step_factor', 0.)
+
+
+    nan_check(rays_o)
+    nan_check(rays_d)
+    nan_check(hits_t)
+
+
+
     results = {}
     (rays_a, xyzs, dirs,
     results['deltas'], results['ts'], results['rm_samples']) = \
@@ -222,6 +230,10 @@ def __render_rays_train(model, rays_o, rays_d, hits_t, **kwargs):
             rays_o, rays_d, hits_t[:, 0], model.density_bitfield[time_grid_indx],
             model.cascades, model.scale,
             exp_step_factor, model.grid_size, MAX_SAMPLES)
+    nan_dict_check(results)
+    nan_check(rays_a)
+
+
     #print(f'rays_a,rays_a.shape',rays_a,rays_a.shape)
     #print(kwargs)
     for k, v in kwargs.items():
@@ -233,6 +245,10 @@ def __render_rays_train(model, rays_o, rays_d, hits_t, **kwargs):
 
             kwargs[k] = torch.repeat_interleave(v[rays_a[:, 0]], rays_a[:, 2], 0)
     sigmas, rgbs,extra = model(xyzs, dirs, **kwargs)
+
+    nan_check(sigmas)
+    nan_check(rgbs)
+    nan_dict_check(extra)
 
     (results['vr_samples'], results['opacity'],
     results['depth'], results['rgb'], results['ws']) = \
@@ -275,5 +291,5 @@ def __render_rays_train(model, rays_o, rays_d, hits_t, **kwargs):
 
     results['sigma_entropy'] = sigma_entropy_function(sigmas)
 
-
+    nan_dict_check(results)
     return results

train_dynamic.py
@@ -33,7 +33,7 @@ from models.rendering_time import render,MAX_SAMPLES
 from apex.optimizers import FusedAdam
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from losses import NeRFLoss,loss_sum,dict_sum
-
+from torch.optim.adam import Adam
 # metrics
 from torchmetrics import (
     PeakSignalNoiseRatio, 
@@ -51,7 +51,7 @@ from pytorch_lightning.callbacks import GradientAccumulationScheduler
 from utils import slim_ckpt, load_ckpt
 from dyna_datasets.ray_utils import visualize_poses
 #import warnings; warnings.filterwarnings("ignore")
-
+from models.debug_utils import nan_dict_check,nan_check
 
 visualize_poses_flag=False
 import warnings
@@ -244,7 +244,9 @@ class DNeRFSystem(LightningModule):
             if n not in ['dR', 'dT']: net_params += [p]
 
         opts = []
-        self.net_opt = FusedAdam(net_params, self.hparams.lr, eps=1e-15)
+        self.net_opt = Adam(net_params, self.hparams.lr,eps=1e-15,fused=True,amsgrad=True) # ngp_pl repository set eps  1e-15, but may result in numerical error
+        #self.net_opt = FusedAdam(net_params, self.hparams.lr,eps=1e-15) # ngp_pl repository set eps  1e-15, but may result in numerical error
+
         opts += [self.net_opt]
         if self.hparams.optimize_ext:
             opts += [FusedAdam([self.dR, self.dT], 1e-6)] # learning rate is hard-coded
@@ -322,6 +324,8 @@ class DNeRFSystem(LightningModule):
                                    loss_B=self.loss(results_trunk, batch,use_dst_loss=self.global_step>=self.distortion_loss_step)
                                        )
 
+            nan_dict_check(summed_loss_trunk)
+
             named_results=dict_sum(named_results,results_trunk,keys=['vr_samples','rm_samples'])
             with torch.no_grad():
                 psnr_sum+=self.train_psnr(results_trunk['rgb'], batch['rgb'][start_:end_])
@@ -335,6 +339,10 @@ class DNeRFSystem(LightningModule):
                 0.5*(unit_exposure_rgb-self.train_dataset.unit_exposure_rgb)**2
         loss = sum(lo.mean() for lo in summed_loss_trunk.values()) / trunk_numbers
 
+        try:
+            nan_check(loss)
+        except:
+            exit(10)
         '''
         calculate every trunk psnr and average them.
         '''
@@ -479,12 +487,14 @@ def print_time_elapse(t0,t1,prefix=''):
 
 
 if __name__ == '__main__':
-    torch.autograd.set_detect_anomaly(True)
+
     device_=torch.cuda.get_device_name(0)
+    torch.backends.cuda.matmul.allow_tf32=True
     #assert device_.endswith('3090')
     #torch.cuda.memory_summary(device=None, abbreviated=False)
     hparams = get_opts()
     t_start=datetime.datetime.now()
+    torch.set_float32_matmul_precision('highest')
 
     print(f'{datetime.datetime.now()}')
     print(f'configs={hparams}')
@@ -524,7 +534,6 @@ if __name__ == '__main__':
     print(f'{datetime.datetime.now()},start traning.')
     #torch._dynamo.config.verbose = True
     #torch._dynamo.config.suppress_errors = True
-    #torch.set_float32_matmul_precision('highest')
     trainer.fit(compiled_system, ckpt_path=hparams.ckpt_path)
 
     t1=datetime.datetime.now()